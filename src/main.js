c201a982a46cf6f00f684eb70ea89fc0165a11cd
antoxer <malyarov99@mail.ru>
2020-07-29 22:04:10 +1000

    Сделана валидация

--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
 import Vue from 'vue';
-import Vuelidate from 'vuelidate/src';
+import Vuelidate from 'vuelidate';
+import Axios from 'axios';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
+Vue.prototype.$http = Axios;
+Vue.prototype.$http.defaults.baseURL = process.env.BACKEND_DOMAIN;
 
 Vue.use(Vuelidate);
+
+const token = localStorage.getItem('token');
+if (token) {
+  Vue.prototype.$http.defaults.headers.common.Authorization = `Bearer ${token}`;
+}
 
 new Vue({
   router,
