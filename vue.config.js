95577514aa5ff9db67ea5ec3125a9fe8d0ccf362
Антон Маляров <anton.maliarov@fogstream.ru>
2020-07-29 14:13:34 +1000

    Деплой на github pages

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,7 @@ module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
-  publicPath: '/stms_client/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/stms_client/'
+    : '/',
 };
