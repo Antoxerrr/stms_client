81d15f86ef44469caf4399f328b3cded19c57579
antoxer <malyarov99@mail.ru>
2020-08-07 02:35:06 +1000

    Прикручены запросы и лоадинг

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import auth from './auth.module';
+import loading from './loading.module';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     auth,
+    loading,
   },
 });
